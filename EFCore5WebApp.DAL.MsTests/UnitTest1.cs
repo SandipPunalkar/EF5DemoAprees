bb42c55fde9cf612f06ea1fa661553874918171e
Sandip Punalkar <sandippunalkar9@gmail.com>
2022-09-20 10:28:45 +0530

    Add project files.

---
+++ EFCore5WebApp.DAL.MsTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EFCore5WebApp.DAL.MsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
